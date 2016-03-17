63faa1ffe4346d453351ed37e3a2be219895be62
汪洋 <wangyang@wangyangdemacbook-pro.local>
2016-03-17 18:05:28 +0800

    commit code

---
+++ henews/henews/class/cell/newsCell/NewsLinkCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsLinkCell.h
+//  henews
+//
+//  Created by 汪洋 on 16/3/17.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsLinkCell : UITableViewCell
+
+@end
---
+++ henews/henews/class/cell/newsCell/NewsLinkCell.m
@@ -0,0 +1,23 @@
+//
+//  NewsLinkCell.m
+//  henews
+//
+//  Created by 汪洋 on 16/3/17.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import "NewsLinkCell.h"
+
+@implementation NewsLinkCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
