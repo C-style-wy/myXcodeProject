9a062e0c9e7398a29571d98a94d878e71bb83e67
汪洋 <wangyang@wangyangdemacbook-pro.local>
2016-03-17 17:52:16 +0800

    commit code

---
+++ henews/henews/model/newsMode/NewsLinkCellData.h
@@ -0,0 +1,13 @@
+//
+//  NewsLinkCellData.h
+//  henews
+//
+//  Created by 汪洋 on 16/3/17.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsLinkCellData : NSObject
+
+@end
---
+++ henews/henews/model/newsMode/NewsLinkCellData.m
@@ -0,0 +1,13 @@
+//
+//  NewsLinkCellData.m
+//  henews
+//
+//  Created by 汪洋 on 16/3/17.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import "NewsLinkCellData.h"
+
+@implementation NewsLinkCellData
+
+@end
