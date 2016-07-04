2389f9b7e15d569c30405ab2206a62f146683380
汪洋 <wangyang@wangyangdemacbook-pro.local>
2016-07-04 14:30:40 +0800

    commit code

---
+++ henews250/henews250/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  henews250
+//
+//  Created by 汪洋 on 16/7/4.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ henews250/henews250/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  henews250
+//
+//  Created by 汪洋 on 16/7/4.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
