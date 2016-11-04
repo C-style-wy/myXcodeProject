b1deaa3500a801b4215642fc28f9f3f4a938e674
C-style-wy <417815802@qq.com>
2016-11-04 14:40:50 +0800

    commit code

---
+++ henews250/henews250/Model/NewsDetailMode/DetailShowMode/NewsSectionMode.h
@@ -0,0 +1,15 @@
+//
+//  NewsSectionMode.h
+//  henews250
+//
+//  Created by 汪洋 on 2016/11/2.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import "BaseMode.h"
+
+@interface NewsSectionMode : BaseMode
+
+
+
+@end
---
+++ henews250/henews250/Model/NewsDetailMode/DetailShowMode/NewsSectionMode.m
@@ -0,0 +1,15 @@
+//
+//  NewsSectionMode.m
+//  henews250
+//
+//  Created by 汪洋 on 2016/11/2.
+//  Copyright © 2016年 汪洋. All rights reserved.
+//
+
+#import "NewsSectionMode.h"
+
+@implementation NewsSectionMode
+
+
+
+@end
