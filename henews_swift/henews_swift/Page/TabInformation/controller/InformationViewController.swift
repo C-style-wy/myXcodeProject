972d065a02fad5053f4ac77da99e838969d3f377
C-style-wy <417815802@qq.com>
2017-01-04 15:46:20 +0800

    commit code

---
+++ henews_swift/henews_swift/Page/TabInformation/controller/InformationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InformationViewController.swift
+//  henews_swift
+//
+//  Created by 汪洋 on 2017/1/4.
+//  Copyright © 2017年 汪洋. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ henews_swift/henews_swift/Page/TabRecommend/controller/RecommendViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RecommendViewController.swift
+//  henews_swift
+//
+//  Created by 汪洋 on 2017/1/4.
+//  Copyright © 2017年 汪洋. All rights reserved.
+//
+
+import UIKit
+
+class RecommendViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
